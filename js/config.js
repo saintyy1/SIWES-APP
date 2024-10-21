57c1c34e86498cb0437e4e3e388031c084677010
tolu <olufowobirasaq69@gmail.com>
2024-10-21 13:26:20 +0100

    Add config.js back to the repository

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBj_CQ6rMnGely-gFpelqHdwhBrsFLXxhM",
+    authDomain: "ninja-firestore-32820.firebaseapp.com",
+    databaseURL: "https://ninja-firestore-32820-default-rtdb.firebaseio.com",
+    projectId: "ninja-firestore-32820",
+    storageBucket: "ninja-firestore-32820.appspot.com",
+    messagingSenderId: "404772243116",
+    appId: "1:404772243116:web:cf4f8e79e6156130cf0109",
+    measurementId: ""
+};
+
+export default firebaseConfig;
